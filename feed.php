893c0a66abec5adb06395c9d7211b643e5f08758
TheCodeCronicles <fijanko321@gmail.com>
2025-05-02 09:49:58 +0200

    Header Logo Added

--- feed.php
+++ feed.php
@@ -39,7 +39,7 @@ $result = $stmt->get_result();
 </head>
 <body>
     <header>
-        <h1>[REDACTED] Feed</h1>
+        <h1><img src="assets\images\[REDACTED].png" height="50"></h1>
         <p>Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?>!</p>
         <a href="post.php">Create Post</a> | <a href="logout.php">Logout</a>
     </header>
